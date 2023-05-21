781abe8096bf4ece1d4d41fc2ef56bf293e68844
Dean Rumsby <dean@deanrumsby.com>
2023-05-21 09:53:59 +0100

    feat: add gitsigns
    Adding gitsigns means that we can visibly see where changes have been made within a buffer.
    It also allows us to cycle across the hunks and reset them from within the buffer.

--- nvim/.config/nvim/lua/deanrumsby/plugins/git.lua
+++ nvim/.config/nvim/lua/deanrumsby/plugins/git.lua
@@ -4,4 +4,30 @@ return {
 
 	-- github extras
 	"tpope/vim-rhubarb",
+
+	-- gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(bufnr)
+				vim.keymap.set(
+					"n",
+					"[h",
+					require("gitsigns").prev_hunk,
+					{ buffer = bufnr, desc = "Go to Previous [H]unk" }
+				)
+				vim.keymap.set("n", "]h", require("gitsigns").next_hunk, { buffer = bufnr, desc = "Go to Next [H]unk" })
+				vim.keymap.set(
+					"n",
+					"<leader>gp",
+					require("gitsigns").preview_hunk,
+					{ buffer = bufnr, desc = "[G]it [P]review Hunk" }
+				)
+				vim.keymap.set("n", "<leader>gr", require("gitsigns").reset_hunk, {
+					buffer = bufnr,
+					desc = "[G]it [R]eset Hunk",
+				})
+			end,
+		},
+	},
 }
