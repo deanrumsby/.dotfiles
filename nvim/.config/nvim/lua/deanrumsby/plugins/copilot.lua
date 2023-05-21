ca7056574ef869b7448fa035baaa4728db4f47a7
Dean Rumsby <dean@deanrumsby.com>
2023-05-21 20:58:35 +0100

    feat: reset copilot map to default
    Now that I have the tab key available again, I will use that for copilot, to keep in sync across the editors I use.

--- nvim/.config/nvim/lua/deanrumsby/plugins/copilot.lua
+++ nvim/.config/nvim/lua/deanrumsby/plugins/copilot.lua
@@ -1,11 +1,3 @@
 return {
 	"github/copilot.vim",
-	init = function()
-		vim.g.copilot_no_tab_map = true
-	end,
-	config = function()
-		vim.cmd([[
-			imap <silent><script><expr> <M-space> copilot#Accept("")
-		]])
-	end,
 }
