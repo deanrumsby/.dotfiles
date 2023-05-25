ab3b211ecd38230099104038cdd7be3b3646d2a1
Dean Rumsby <dean@deanrumsby.com>
2023-05-25 22:01:06 +0100

    feat: add keymaps to disable and enable diagnostics
    Helpful when working on projects where diagnostics messages have gone unkept, and we need to declutter the working space temporarily

--- nvim/.config/nvim/lua/deanrumsby/mappings.lua
+++ nvim/.config/nvim/lua/deanrumsby/mappings.lua
@@ -3,6 +3,12 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>dd", function()
+	vim.diagnostic.disable()
+end, { desc = "[D]iagnostics [D]isable" })
+vim.keymap.set("n", "<leader>de", function()
+	vim.diagnostic.enable()
+end, { desc = "[D]iagnostics [E]nable" })
 
 -- terminal keymaps
 vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], { silent = true, noremap = true })
