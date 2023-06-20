74739a057dd9fe7ca54d12ecfa0b2d51caf5b272
Dean Rumsby <dean@deanrumsby.com>
2023-06-20 06:25:15 +0100

    feat: update cargo script

--- scripts/.config/scripts/cargo.sh
+++ scripts/.config/scripts/cargo.sh
@@ -9,3 +9,9 @@ cargo install stylua
 
 # wasm-pack
 cargo install wasm-pack
+
+# useful for creating UF2 images for the RP2040 USB Bootloader
+cargo install elf2uf2-rs --locked
+
+# useful for flashing over the SWD pins using a supported JTAG probe
+cargo install probe-run
