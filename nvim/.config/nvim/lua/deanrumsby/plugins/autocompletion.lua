f665b7ceeb4a2573a05427bbceb63b2f1db6c295
Dean Rumsby <dean@deanrumsby.com>
2024-05-03 14:23:30 +0100

    feat: remove LuaSnip

--- nvim/.config/nvim/lua/deanrumsby/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/deanrumsby/plugins/autocompletion.lua
@@ -14,18 +14,11 @@ return {
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
 		},
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
 
 			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -35,7 +28,6 @@ return {
 				}),
 				sources = {
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
 				},
 			})
 		end,
